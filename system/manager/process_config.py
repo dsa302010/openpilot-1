9e4fce18832296c4f2ca638c4b3da0aaa0adaa05
herizon1054 <spivbaby@gmail.com>
2025-12-09 02:13:23 +0800

    Add files via upload

--- system/manager/process_config.py
+++ system/manager/process_config.py
@@ -71,7 +71,7 @@ procs = [
   DaemonProcess("manage_athenad", "system.athena.manage_athenad", "AthenadPid"),
 
   NativeProcess("loggerd", "system/loggerd", ["./loggerd"], logging),
-  NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
+  #NativeProcess("encoderd", "system/loggerd", ["./encoderd"], only_onroad),
   NativeProcess("stream_encoderd", "system/loggerd", ["./encoderd", "--stream"], or_(notcar, and_(dashy_with_video, only_onroad))),
   PythonProcess("logmessaged", "system.logmessaged", always_run),
 
