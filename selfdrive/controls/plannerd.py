8154bd02a12edc4b978497964e7e316180a1570d
herizon1054 <spivbaby@gmail.com>
2025-12-16 02:58:11 +0800

    Add files via upload

--- selfdrive/controls/plannerd.py
+++ selfdrive/controls/plannerd.py
@@ -7,10 +7,8 @@ from openpilot.selfdrive.controls.lib.ldw import LaneDepartureWarning
 from openpilot.selfdrive.controls.lib.longitudinal_planner import LongitudinalPlanner, DPFlags
 import cereal.messaging as messaging
 
-
 def main():
   config_realtime_process(5, Priority.CTRL_LOW)
-
   cloudlog.info("plannerd is waiting for CarParams")
   params = Params()
   CP = messaging.log_from_bytes(params.get("CarParams", block=True), car.CarParams)
@@ -19,29 +17,30 @@ def main():
   ldw = LaneDepartureWarning()
   longitudinal_planner = LongitudinalPlanner(CP)
   pm = messaging.PubMaster(['longitudinalPlan', 'driverAssistance'])
-  sm = messaging.SubMaster(['carControl', 'carState', 'controlsState', 'liveParameters', 'radarState', 'modelV2', 'selfdriveState'],
+
+  # USE gpsLocationExternal
+  sm = messaging.SubMaster(['carControl', 'carState', 'controlsState', 'liveParameters',
+                            'radarState', 'modelV2', 'selfdriveState', 'gpsLocationExternal'],
                            poll='modelV2')
 
   dp_flags = 0
-  if params.get_bool("dp_lon_acm"):
-    dp_flags |= DPFlags.ACM
-  if params.get_bool("dp_lon_aem"):
-    dp_flags |= DPFlags.AEM
-  if params.get_bool("dp_lon_dtsc"):
-    dp_flags |= DPFlags.DTSC
+  if params.get_bool("dp_lon_acm"): dp_flags |= DPFlags.ACM
+  if params.get_bool("dp_lon_aem"): dp_flags |= DPFlags.AEM
+  if params.get_bool("dp_lon_dtsc"): dp_flags |= DPFlags.DTSC
+  if params.get_bool("dp_lon_scda"): dp_flags |= DPFlags.SCDA
+
+
   while True:
     sm.update()
     if sm.updated['modelV2']:
       longitudinal_planner.update(sm, dp_flags)
       longitudinal_planner.publish(sm, pm)
-
       ldw.update(sm.frame, sm['modelV2'], sm['carState'], sm['carControl'])
       msg = messaging.new_message('driverAssistance')
       msg.valid = sm.all_checks(['carState', 'carControl', 'modelV2', 'liveParameters'])
       msg.driverAssistance.leftLaneDeparture = ldw.left
       msg.driverAssistance.rightLaneDeparture = ldw.right
       pm.send('driverAssistance', msg)
-
 
 if __name__ == "__main__":
   main()
