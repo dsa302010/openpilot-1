fda01bc3a60e42ebe0d9cd094fe6a5f98675f685
herizon1054 <spivbaby@gmail.com>
2026-01-06 14:59:34 +0800

    Update manager.py

--- system/manager/manager.py
+++ system/manager/manager.py
@@ -66,6 +66,7 @@ def manager_init() -> None:
   params.put_bool("IsTestedBranch", build_metadata.tested_channel)
   params.put_bool("IsReleaseBranch", build_metadata.release_channel)
   params.put("HardwareSerial", serial)
+  params.put("LanguageSetting", "main_zh-CHT")
 
   # set dongle id
   reg_res = register(show_spinner=True)
