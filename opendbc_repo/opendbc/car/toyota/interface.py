e4ec3de9feb98c0d5021eb8c02941e019cdb8395
herizon1054 <spivbaby@gmail.com>
2026-02-02 00:57:57 +0800

    修改煞停頓挫感 來自dsa302010

--- opendbc_repo/opendbc/car/toyota/interface.py
+++ opendbc_repo/opendbc/car/toyota/interface.py
@@ -181,9 +181,10 @@ class CarInterface(CarInterfaceBase):
 
     if candidate in TSS2_CAR:
       ret.flags |= ToyotaFlags.RAISED_ACCEL_LIMIT.value
-
+#預設0.25
       ret.vEgoStopping = 0.15
       ret.vEgoStarting = 0.25
+#預設0.3     
       ret.stoppingDecelRate = 0.15  # reach stopping target smoothly
 
       # Hybrids have much quicker longitudinal actuator response
