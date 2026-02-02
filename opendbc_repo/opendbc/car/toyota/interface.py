8bdd3db87d356458ce20686089aec5a86f9138d2
herizon1054 <spivbaby@gmail.com>
2026-02-02 17:35:26 +0800

    回覆原版

--- opendbc_repo/opendbc/car/toyota/interface.py
+++ opendbc_repo/opendbc/car/toyota/interface.py
@@ -182,10 +182,10 @@ class CarInterface(CarInterfaceBase):
     if candidate in TSS2_CAR:
       ret.flags |= ToyotaFlags.RAISED_ACCEL_LIMIT.value
 #預設0.25
-      ret.vEgoStopping = 0.15
+      ret.vEgoStopping = 0.25
       ret.vEgoStarting = 0.25
 #預設0.3     
-      ret.stoppingDecelRate = 0.15  # reach stopping target smoothly
+      ret.stoppingDecelRate = 0.3  # reach stopping target smoothly
 
       # Hybrids have much quicker longitudinal actuator response
       if ret.flags & ToyotaFlags.HYBRID.value:
